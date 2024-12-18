85d6beb0c010743071d548c2830f7fc1e80aa417
Kha Tran <kha.tran@paysmart.com.vn>
2024-12-18 10:16:37 +0700

    hackerrank_recursiveDigitSum

--- src/hackerRank/recursiveDigitSum/SuperDigit.swift
+++ src/hackerRank/recursiveDigitSum/SuperDigit.swift
@@ -1,18 +1,10 @@
 func superDigit(n: String, k: Int) -> Int {
-    // Helper function to calculate the super digit of a number
-    func calculateSuperDigit(_ num: Int) -> Int {
-        if num < 10 {
-            return num
-        }
-        return calculateSuperDigit(num.reduce(0, { $0 + $1 }))
+    var digitSum = n.reduce(0) { $0 + Int(String($1))! } * k
+
+    // Reduce the sum until it becomes a single-digit number
+    while digitSum > 10 {
+        digitSum = String(digitSum).reduce(0) { $0 + Int(String($1))! }
     }
-    
-    // Calculate the sum of digits of n
-    let initialSum = n.reduce(0) { $0 + Int(String($1))! }
-    
-    // Calculate the initial number by multiplying the sum of digits of n with k
-    let initialNum = initialSum * k
-    
-    // Use the recursive function to calculate the super digit of the initial number
-    return calculateSuperDigit(initialNum)
+
+    return digitSum
 }
