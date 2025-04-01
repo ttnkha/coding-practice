200ec2b3781a576e0e78d0b79f0f96db2e52f760
Kha Tran <kha.tran@paysmart.com.vn>
2025-04-01 11:24:25 +0700

    hackerrank_marsExploration

--- src/hackerRank/marsExploration/MarsExploration.swift
+++ src/hackerRank/marsExploration/MarsExploration.swift
@@ -2,8 +2,7 @@ func marsExploration(s: String) -> Int {
     var count = 0
     
     for (index, char) in s.enumerated() {
-        // Check if the character matches the expected "SOS" pattern
-        if char != "SOS"[(index % 3)] {
+        if char != Array("SOS")[(index % 3)] {
             count += 1
         }
     }
